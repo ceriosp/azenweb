63a113e46a6d7a6aa164737ae60ee6fd3f73c07d
Luis Felipe <luisfe_617@outlook.com>
2017-08-26 19:55:02 -0500

    logica de caso de uso Lanzar aplicacion implementada

--- src/modules/app/components/ZListadoAplicaciones.tsx
+++ src/modules/app/components/ZListadoAplicaciones.tsx
@@ -68,7 +68,6 @@ export class ZListadoAplicaciones extends React.Component<OwnProps & ConnectedSt
                 )}
 
                 {this.props.zLoginModule.zAplList.apls.length > 0 && (
-
                     this.renderAplList(this.props.zLoginModule)
                 )}
             </div>
@@ -79,40 +78,61 @@ export class ZListadoAplicaciones extends React.Component<OwnProps & ConnectedSt
         this.props.lanzarAplicacion(e.target.value);
     }
 
+    /**
+     * 50 applications - 8 applications by row
+     * las two applications in the third row?
+     * Every row has to have a maxwith {1} eg. -> <Thumbail xs={1} sm={1} md={1} lg={1}>
+     */
     private renderAplList(zLoginModule: IZLoginModule) {
 
-        let thumbnails: any = [];
+        let zAplRows: Array<JSX.Element> = [];
+        let rowsNumber = Math.ceil(zLoginModule.zAplList.apls.length / 8);
 
-        zLoginModule.zAplList.apls.forEach((apl: IZApl, index: number) => {
+        let currentApl: number = 0;
 
-            let thumbnail = (
-                <Col xs={2} sm={2} md={2} lg={2}>
-                    <Thumbnail src="dist/img/azenLogo.jpg">
-                        <p>{apl.descr}</p>
-                        <p>
-                            <Button id="" bsStyle="primary" href="#" target="_blank">Inciar</Button>
-                        </p>
-                    </Thumbnail>
-                </Col>
-            );
+        for (let i = 0; i < rowsNumber; i++) {
 
-            thumbnails.push(thumbnail);
-        });
+            let zAplColumns: Array<JSX.Element> = [];
 
-        const aplicationsGrid = (
-            <Grid>
+            for (let j = 0; j < zLoginModule.zAplList.apls.length; j++) {
+
+                zAplColumns.push(
+                    <Col key={'zAplCol' + currentApl} xs={1} sm={1} md={1} lg={1}>
+                        <Thumbnail src="dist/img/azenLogo.jpg">
+                            <p>{zLoginModule.zAplList.apls[currentApl].descr}</p>
+                            <p>
+                                <Button
+                                    bsStyle="primary"
+                                    onClick={this.lanzarAplicacion}
+                                    value={zLoginModule.zAplList.apls[currentApl].apl}
+                                    href={'?idApl=' + zLoginModule.zAplList.apls[currentApl].apl}
+                                    target="_blank">
+                                    Inciar
+                                </Button>
+                            </p>
+                        </Thumbnail>
+                    </Col>
+                );
+                currentApl++;
+
+                if (j == 7) break;
+            }
+
+            zAplRows.push(<Row key={'zAplRow' + i}>{zAplColumns}</Row>);
+        }
+
+        const zAplsContainer: JSX.Element = (
+            <Grid fluid>
                 <Row>
                     <Col xs={12} sm={12} md={12} lg={12}>
-                        <Row>
-                            {thumbnails}
-                        </Row>
+                        {zAplRows}
                     </Col>
                 </Row>
             </Grid>
         );
 
         return (
-            aplicationsGrid
+            zAplsContainer
         );
     }
-}
+} 7
